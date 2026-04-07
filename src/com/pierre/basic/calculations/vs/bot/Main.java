5d2e2a5c9b60efcc2617eb4727caeb373b2812fc
PierreSenpai <pierre07.walther@gmail.com>
2026-04-08 00:50:03 +0200

    Refactor question array to static attribute

--- src/com/pierre/basic/calculations/vs/bot/Main.java
+++ src/com/pierre/basic/calculations/vs/bot/Main.java
@@ -3,9 +3,9 @@ package com.pierre.basic.calculations.vs.bot;
 import java.util.Scanner;
 
 public class Main {
-    public Question[] questions;
+    public static Question[] questions;
 
-    public Main(int nQuestions) {
+    private static void generateQuestions(int nQuestions) {
         questions = new Question[nQuestions];
         for (int i = 0; i < nQuestions; i++) {
             questions[i] = new Question();
@@ -14,14 +14,14 @@ public class Main {
 
     public static void main(String[] args) {
         int nQuestions = 20;
-        Scanner scanner = new Scanner(System.in);
+        generateQuestions(nQuestions);
 
-        Main game = new Main(nQuestions);
+        Scanner scanner = new Scanner(System.in);
         User user = new User();
         Bot bot = new Bot(nQuestions);
 
         // test output
-        for (Question q : game.questions) {
+        for (Question q : questions) {
             System.out.println(q.askQuestion(scanner));
         }
 
