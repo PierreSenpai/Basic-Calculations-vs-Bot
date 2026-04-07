22851b0b07f8de2d764314e13a8824338a41e07e
PierreSenpai <pierre07.walther@gmail.com>
2026-04-08 00:07:21 +0200

    Add division

--- src/com/pierre/basic/calculations/vs/bot/Question.java
+++ src/com/pierre/basic/calculations/vs/bot/Question.java
@@ -7,32 +7,43 @@ public class Question {
     public char op;
     public int res;
 
-    private static char[] operators = {'+', '-', '*'};
+    private static char[] operators = {'+', '-', '*', '/'};
 
     public Question() {
-        // op: 0 = add, 1 = sub, 2 = mul
-        int opNum = (int) Math.round(Math.random() * 2);
+        // op: 0 = add, 1 = sub, 2 = mul, 3 = div
+        int opNum = (int) Math.round(Math.random() * 3);
         op = operators [opNum];
+        int num1, num2;
 
-        // numbers
-        int num1 = (int) Math.round(Math.random() * 40) - 20;
-        int num2 = (int) Math.round(Math.random() * 40) - 20;
+        // loop to ensure that division has nice result
+        while (true) {
+            // numbers
+            num1 = (int) Math.round(Math.random() * 40) - 20;
+            num2 = (int) Math.round(Math.random() * 40) - 20;
 
-        // calculate result
-        switch (op) {
-            case '+':
-                res = num1 + num2;
-                break;
-            case '-':
-                res = num1 - num2;
-                break;
-            case '*':
-                res = num1 * num2;
-                break;
-            default:
-                res = 0;
+            // calculate result
+            switch (op) {
+                case '+':
+                    res = num1 + num2;
+                    break;
+                case '-':
+                    res = num1 - num2;
+                    break;
+                case '*':
+                    res = num1 * num2;
+                    break;
+                case '/':
+                    // division by zero
+                    if (num2 == 0) {continue;}
+                    res = num1 / num2;
+                    // if division results in truncated float
+                    if (res * num2 != num1) {continue;}
+                    break;
+                default:
+                    res = 0;
+            }
+            break;
         }
-
         // surround negative numbers with parentheses
         if (num1 < 0) {
             a = "(" + num1 + ")";
